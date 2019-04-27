3322d86bb6aab5f14d21bceaf1f003b33e8c89cd
Wassam <wassam@wassam.com>
2019-04-27 04:29:06 -0400

    Initial Commit

--- To Do List/FirstViewController.swift
+++ To Do List/FirstViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+
+    @IBOutlet var tableView: UITableView!
+    
+    var todoList:[String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
+    override func viewDidAppear(_ animated: Bool) {
+        let todoListObj = UserDefaults.standard.object(forKey: "todoList")
+        
+        if let todoListItems = todoListObj as? [String] {
+            todoList = todoListItems
+            tableView.reloadData()
+        }
+        //tableView.reloadData()
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+      
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCell.EditingStyle.delete {
+            todoList.remove(at: indexPath.row)
+            tableView.reloadData()
+            UserDefaults.standard.set(todoList, forKey: "todoList")
 
+        }
+    }
+    func addTask( newTask: String){
+        todoList.append(newTask)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "Cell")
+        cell.textLabel?.text  = todoList[indexPath.row]
+        
+            
+        return cell
+    }
 
 }
 
--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController, UITextFieldDelegate {
 
+
+    @IBOutlet weak var newTaskTextField: UITextField!
+    @IBAction func addTaskButton(_ sender: Any) {
+        let todoListObj = UserDefaults.standard.object(forKey: "todoList")
+        var todoList:[String] = []
+        
+        if let tempTodoList = todoListObj as? [String] {
+            todoList = tempTodoList
+            todoList.append(newTaskTextField.text!)
+            print(todoList)
+        }else {
+            todoList = [newTaskTextField.text!]
+        }
+        UserDefaults.standard.set(todoList, forKey: "todoList")
+        newTaskTextField.text = ""
+        newTaskTextField.placeholder = "add another task"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
 }
 
